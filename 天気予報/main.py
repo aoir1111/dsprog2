f04f58e34fc9d34a59c22299713d33492f0afb5c
aoto <s2422094@stu.musashino-u.ac.jp>
2024-11-27 11:23:32 +0900

    apiを取得するurlを入力

--- 天気予報/main.py
+++ 天気予報/main.py
@@ -1,8 +1,10 @@
 import flet as ft
+import requests
 
-
+data_json = requests.get('http://www.jma.go.jp/bosai/common/const/area.json').json()
+data =requests.get('https://www.jma.go.jp/bosai/forecast/data/forecast/')
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.add(ft.SafeArea(ft.Text("天気予報")))
 
 
 ft.app(main)
